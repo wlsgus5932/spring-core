49bedea384a4523a9ac3e436d19a7c22834983c2
wlsgus5932 <wlsgus1149@naver.com>
2023-09-06 22:31:09 +0900

    관심사의 분리

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,19 +1,29 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
-    MemberService memberService = new MemberServiceImpl();
+
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
         //given
         Member member = new Member(1L, "memberA", Grade.VIP);
+        AppConfig appConfig = new AppConfig();
 
         //when
-        memberService.join(member);
-        Member findMember = memberService.findById(1L);
+        appConfig.memberService().join(member);
+        Member findMember = appConfig.memberService().findById(1L);
 
         //then
         Assertions.assertThat(member).isEqualTo(findMember);
