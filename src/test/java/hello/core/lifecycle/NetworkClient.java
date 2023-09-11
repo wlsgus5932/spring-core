e44e0c5e66fdc91b9ad48b812e19b85690549f57
wlsgus5932 <wlsgus1149@naver.com>
2023-09-11 22:26:57 +0900

    bean 생명주기 @bean(initMethod= , destoryMethod) @Bean이 붙었을때만 사용 가능하다

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url;
 
     public NetworkClient() {
@@ -30,15 +30,13 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close = "+url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("afterPropertiesSet");
+    public void init() {
+        System.out.println("init");
         connect();
         call("초기화 연결 메세지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void destroy() {
         System.out.println("destroy");
         disconnect();
     }
